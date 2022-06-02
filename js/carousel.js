b6b61f3117871577b24a26b8a26d102d0629d8f1
Kien Quan <hotrankienquan144@gmail.com>
2022-06-02 14:17:27 +0700

    first

---
+++ js/carousel.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//   $(".slider-carousel").slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     prevArrow: false,
+//     nextArrow: false,
+//     dots: true, 
+//     autoplay: true,
+//     autoplaySpeed: 4000,   
+//     speed: 1000,           
+//   });
+// });
